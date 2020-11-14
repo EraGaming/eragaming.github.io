8a963ad6a1a661a482b448fbe27f74b37cfeefb9
Todd <todd@willsie.net>
2020-11-13 19:44:26 -0700

    Donation Tracker

---
+++ stream-tools/assets/app.js
@@ -0,0 +1,53 @@
+JF.initialize({apiKey: "8471788e5182df01cf109eb4fa8acb2f"});
+
+formID = 202756478593065
+
+function tickermarquee(){
+    JF.getFormSubmissions(formID,{
+        limit: 200 // filter
+    }, function(response){
+        /**
+         successful response including submissions of form with given id
+        .
+        */
+        var j = 0
+        for(var i=0; i<response.length; i++){
+
+            myObj = JSON.parse(response[i].answers["10"].answer.paymentArray);
+            total = myObj.total;
+
+            if (i == 0){
+                $("#donors").html("");
+            };
+
+            if (total > 0){
+                console.log(response[i].answers);
+                $("#donors").append("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+(response[i].answers["5"].answer.first).replace(/(\r\n|\n|\r)/gm,"")+": $"+total);
+                j++
+            };
+            
+        }
+
+        console.log(j)
+
+        duration = j * 1000;
+
+        console.log(duration)
+        
+        $('.marquee').marquee({
+            //speed in milliseconds of the marquee
+            duration: duration,
+            //gap in pixels between the tickers
+            gap: 0,
+            //time in milliseconds before the marquee will start animating
+            delayBeforeStart: 0,
+            //'left' or 'right'
+            direction: 'left',
+            //true or false - should the marquee be duplicated to show an effect of continues flow
+            duplicated: true
+        });
+    });
+};
+
+tickermarquee();
+setInterval(tickermarquee, 900000);
